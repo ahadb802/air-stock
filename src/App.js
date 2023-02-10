f0104e4067af7af3c299ebaeacaac9b47ce7a8e0
ahadb802 <abdulahadb802@gmail.com>
2023-02-10 08:33:41 +0500

    remove boiler plates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavScroll from './components/Navbar/Navbar';
+import Details from './pages/Detail/Details';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavScroll />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Details/:name/" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
