56747fd8828ac702e99aa2ce5114f72fd87d0cb0
ahadb802 <abdulahadb802@gmail.com>
2023-02-11 00:22:16 +0500

    refator card accoring to design of blue

--- src/components/Card/Cards.js
+++ src/components/Card/Cards.js
@@ -1,41 +1,27 @@
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import PropTypes from 'prop-types';
 
 function Cards({
-  name, rank, priceUsd, symbol, image,
+  priceUsd, symbol, image,
 }) {
   return (
-    <CardGroup>
-      <Card style={{ width: '18rem', color: 'black' }}>
-        <Card.Header className="head">
-          <span>
-            RANK :
-            {rank}
-          </span>
-          <span>
-            {symbol}
-            <img src={image} alt="" />
-          </span>
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>{name}</Card.Title>
+    <Card className="card">
+      <Card.Body className="card-b d-flex">
+        <img src={image} alt="" />
+        <div className="card-bd">
+          <Card.Title className="text">{symbol}</Card.Title>
           <Card.Text>
-            Price : $
+            $
             {Math.round(priceUsd * 100) / 100}
           </Card.Text>
-          <Button variant="info">Details</Button>
-        </Card.Body>
-      </Card>
-    </CardGroup>
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
 Cards.propTypes = {
-  name: PropTypes.string.isRequired,
   symbol: PropTypes.string.isRequired,
-  rank: PropTypes.number.isRequired,
   priceUsd: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
 };
