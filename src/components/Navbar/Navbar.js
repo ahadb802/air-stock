c305f7efc833b6283d2bb4f95b492970e177347f
ahadb802 <abdulahadb802@gmail.com>
2023-02-11 00:22:33 +0500

    refator Nav according to design of blue

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,20 +6,9 @@ import './Navbar.css';
 
 function NavScroll() {
   return (
-    <Navbar expand="lg" className="nav">
-      <Container fluid>
-        <Navbar.Brand href="/" className="logo"><h2> AIR STOCK MARKET</h2></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/" className="links">Home</Nav.Link>
-            <Nav.Link href="/Details" className="links">Details</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+    <Navbar className="nav">
+      <Container fluid className="nav">
+        <Nav.Link href="/" className="links">Home</Nav.Link>
       </Container>
     </Navbar>
   );
